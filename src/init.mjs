e8591ee60009b90ab620e63040b975ee92aa810f
Changhu Cheng <changhu.cheng@shopee.com>
2024-04-17 19:03:17 +0800

    2024-04-17 19:03:17

--- src/init.mjs
+++ src/init.mjs
@@ -5,6 +5,7 @@ import { cp, opendir, readFile, rename, writeFile } from 'node:fs/promises';
 import readline from 'node:readline';
 import { camelString, gracefullyExit, rewriteFile } from './utils.mjs';
 import { createLog } from './print.mjs';
+import { spawnSync } from 'node:child_process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,6 +15,33 @@ const RootPath = join(__dirname, '..');
 export default async function init({ cwd }) {
   try {
     log('Initializing...');
+    
+    const spawnOptions = { cwd, stdio: 'inherit' };
+    spawnSync('pnpm', [
+      '-w',
+      'add',
+      'vue@2',
+    ], spawnOptions);
+    const webpackDevDependencies = [
+      'webpack',
+      'webpack-cli',
+      'webpack-merge',
+      'clean-webpack-plugin',
+      'mini-css-extract-plugin'
+    ];
+    const babelDevDependencies = ['@babel/core', '@babel/preset-env', 'babel-loader'];
+    const tsDevDependencies = ['@types/node', 'typescript', 'ts-loader'];
+    const cssDevDependencies = ['css-loader', 'postcss-loader', 'sass', 'sass-loader', 'style-loader'];
+    const vueDevDependencies = ['vue-loader', 'vue-style-loader'];
+    spawnSync('pnpm', [
+      '-wD',
+      'add',
+      ...webpackDevDependencies,
+      ...babelDevDependencies,
+      ...tsDevDependencies,
+      ...cssDevDependencies,
+      ...vueDevDependencies,
+    ], spawnOptions);
     await cp(join(RootPath, 'template', 'pmc.json'), join(cwd, 'pmc.json'));
     await cp(join(RootPath, 'template', 'tsconfig.json'), join(cwd, 'tsconfig.json'));
     await cp(join(RootPath, 'template', 'webpack-config'), join(cwd, 'webpack-config'), { recursive: true });
